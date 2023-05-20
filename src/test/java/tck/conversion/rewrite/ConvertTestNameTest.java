efed980fd2a21660ecc5340d53ed618b8aa685a8
Scott M Stark <starksm64@gmail.com>
2023-05-20 10:43:36 -0600

    Fix the test name
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- src/test/java/tck/conversion/rewrite/ConvertTestNameTest.java
+++ src/test/java/tck/conversion/rewrite/ConvertTestNameTest.java
@@ -14,7 +14,7 @@ public class ConvertTestNameTest implements RewriteTest {
     }
 
     @Test
-    void expandsExpectedCustomerInfoMethod() {
+    void addTestAnnotation() {
         rewriteRun(
                 java(
                         """
